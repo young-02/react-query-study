5a416eb89fc9eb50bd93d891f2e93e61476ad25e
tyoung <taeyoung917@gmail.com>
2023-07-17 16:32:49 +0900

    infinite-scoll

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { Posts } from "./Posts";
-import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./App.css";
+
+// import { InfinitePeople } from "./people/InfinitePeople";
+import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+
 function App() {
   return (
-    // provide React Query client to App
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Blog Posts</h1>
-        <Posts />
+        <h1>Infinite SWAPI</h1>
+        {/* <InfinitePeople /> */}
+        <InfiniteSpecies />
+        <ReactQueryDevtools initialIsOpen={false} />
       </div>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
